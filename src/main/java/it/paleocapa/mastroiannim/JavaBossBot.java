ff74d0610eaf6ff3a12083d0abed59cee3cd6c17
silviaarnoldi <101811166+silviaarnoldi@users.noreply.github.com>
2023-05-23 07:49:20 +0000

    c

--- src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
+++ src/main/java/it/paleocapa/mastroiannim/JavaBossBot.java
@@ -41,120 +41,76 @@ public class JavaBossBot extends TelegramLongPollingBot {
 	public String getBotUsername() {
 		return botUsername;
 	}
-/*String nomeCognome = "";
-		String nomePanino = "";
-		if (update.hasMessage() && update.getMessage().hasText()) {
-			long chatId = update.getMessage().getChatId();
-			String userText = update.getMessage().getText();
-			//String userText2 = update.getMessage().getText();
-	
-	
-			SendMessage message = new SendMessage();
-			message.setChatId(chatId);
-	
-			if (userText.equalsIgnoreCase("O")) {
-				message.setText("Hai scelto di ordinare. Ecco la lista dei panini disponibili:\n"
-					+ "Panino Hamburger, 2$\n"
-					+ "Panino CordonBlue,3$\n"
-					+ "Panino Prosciutto, 1$\n"
-					+ "Panino Wurstel, 2$\n"
-					+ "Panino Cotoletta, 2$\n"
-					+ "Panino Arrosto, 2$\n"
-					+ "Quale panino desideri?");
-			} else if (userText.equalsIgnoreCase("Panino Hamburger") || userText.equalsIgnoreCase("Panino CordonBlue")
-			|| userText.equalsIgnoreCase("Panino Prosciutto") || userText.equalsIgnoreCase("Panino Wurstel")
-			|| userText.equalsIgnoreCase("Panino Cotoletta") || userText.equalsIgnoreCase("Panino Arrosto")) {
-				nomePanino = userText;
-				message.setText("Inserisci il tuo nome e cognome");  // Modifica il messaggio per richiedere il nome e cognome
-			} else if (!nomePanino.isEmpty()) {
-				nomeCognome = userText;
-				b.aggiungiOrdine(nomeCognome, nomePanino, 2);
-				Ordine o = b.listaCibo.get(nomePanino).getLast();
-				System.out.println("ordine: " + o.getCibo() + ", " + o.getCliente());
-			
-				// Resetta le variabili per consentire nuovi ordini
-				//nomePanino = "";
-				//nomeCognome = "";
-			} else {
-				message.setText("Benvenuto nel Barbot di Silvia, inserisci 'O' per ordinare.");
-			}
-	
-			try {
-				execute(message);
-			} catch (TelegramApiException e) {
-				LOG.error(e.getMessage());
-			}
-		}*/
+
 		@Override
 		public void onUpdateReceived(Update update) {
-			if (update.hasMessage() && update.getMessage().hasText()) {
-				long chatId = update.getMessage().getChatId();
-				String userText = update.getMessage().getText();
-			
+			//scrivi un programma che implmenta un bot che segua i metodi della classe Bar
+			//e che risponda alle seguenti richieste:
+			// /start: inizia la conversazione
+			// /menu: stampa la lista cib
+			// /ordina: ordina un cibo con nomeCognome, nomeCibo e soldi che vengono inseriti
+			// /paga: paga il conto con nomeCognome
+			// /controlla: controlla il conto con nomeCognome
+			// /fine: termina la conversazione
+			//se il messaggio è /start
+			//invia un messaggio di benvenuto
+			//se il messaggio è /menu
+			//invia un messaggio con il menu
+			//se il messaggio è /ordina
+			//invia un messaggio che dice di inserire nomeCognome, nomeCibo e soldi
+			//se il messaggio è /paga
+			//invia un messaggio che dice quanto si deve pagare e cosa ti rimane
+			// inizia a programmare da qui
+
+			if(update.hasMessage() && update.getMessage().hasText()) {
+				String message_text = update.getMessage().getText();
+				long chat_id = update.getMessage().getChatId();
 				SendMessage message = new SendMessage();
-				message.setChatId(String.valueOf(chatId));
-				
-				if (userText.equals("/start")) {
-					message.setText("Benvenuto nel BarBot di Silvia, per iniziare e vedere il menu scrivi /menu");
-				} else if (userText.equals("/menu")) {
-					StringBuilder menuText = new StringBuilder("Ecco il menu:\n");
-					for (Cibo cibo : Bar.cib) {
-						menuText.append("- ").append(cibo.getNome()).append(": €").append(cibo.getPrezzo()).append("\n");
-					}
-					message.setText(menuText.toString());
-					try {
-						execute(message);
-					} catch (TelegramApiException e) {
-						e.printStackTrace();
-					}
-					SendMessage orderMessage = new SendMessage();
-					orderMessage.setChatId(String.valueOf(chatId));
-					orderMessage.setText("Per ordinare scrivi /ordina");
-					try {
-						execute(orderMessage);
-					} catch (TelegramApiException e) {
-						e.printStackTrace();
-					}
-				} else if (userText.equals("/ordina")) {
-					// Richiesta al cliente di inserire NomeCognome, NomePanino
-					SendMessage orderRequestMessage = new SendMessage();
-					orderRequestMessage.setChatId(String.valueOf(chatId));
-					orderRequestMessage.setText("Inserisci NomeCognome, NomePanino, soldi (es. Mario Rossi, Panino Prosciutto, 5)");
-					try {
-						execute(orderRequestMessage);
-					} catch (TelegramApiException e) {
-						e.printStackTrace();
-					}
-				} else {
-					// Gestione dell'input dell'utente per l'ordinazione
-					String username = update.getMessage().getFrom().getUserName();
-					String[] orderData = userText.split(", ");
-					if (orderData.length == 2) {
-						String nomeCognome = orderData[0];
-						String nomePanino = orderData[1];
-						int soldi=Integer.parseInt(orderData[2]);
-						b.aggiungiOrdine(nomeCognome, nomePanino, soldi);
-						SendMessage orderConfirmationMessage = new SendMessage();
-						orderConfirmationMessage.setChatId(String.valueOf(chatId));
-						orderConfirmationMessage.setText("Hai ordinato: " + nomePanino);
-						try {
-							execute(orderConfirmationMessage);
-						} catch (TelegramApiException e) {
-							e.printStackTrace();
-						}
-					} else {
-						SendMessage errorMessage = new SendMessage();
-						errorMessage.setChatId(String.valueOf(chatId));
-						errorMessage.setText("Input non valido. Inserisci NomeCognome, NomePanino (es. Mario Rossi, Panino Prosciutto)");
-						try {
-							execute(errorMessage);
-						} catch (TelegramApiException e) {
-							e.printStackTrace();
-						}
-			
-					}
+				message.setChatId(String.valueOf(chat_id));
+				message.setText(message_text);
+				try {
+					execute(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
 				}
 			}
+			//inizia con il messaggio di benvenuto
+			if(update.getMessage().getText().equals("/start")){
+				SendMessage message = new SendMessage();
+				message.setChatId(String.valueOf(update.getMessage().getChatId()));
+				message.setText("Benvenuto nel bar di JavaBossBot");
+				try {
+					execute(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+			//se il messaggio è /menu
+			if(update.getMessage().getText().equals("/menu")){
+				SendMessage message = new SendMessage();
+				message.setChatId(String.valueOf(update.getMessage().getChatId()));
+				message.setText("Menu: " + b.getMenu());
+				try {
+					execute(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+			//se il messaggio è /ordina
+			if(update.getMessage().getText().equals("/ordina")){
+				SendMessage message = new SendMessage();
+				message.setChatId(String.valueOf(update.getMessage().getChatId()));
+				message.setText("Inserisci nomeCognome, nomeCibo e soldi");
+				try {
+					execute(message);
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
+			}
+			
+
+			
+
 		}
 }
 
